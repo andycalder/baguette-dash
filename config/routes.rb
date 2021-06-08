ae8c7e8a3caeb9c27393c98d22aa746cb53a54ff
Jorge Arias <jorgeariasgil@gmail.com>
2021-06-08 12:20:08 +1000

    meals controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'meals/index'
+  get 'meals/show'
+  get 'meals/new'
+  get 'meals/edit'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
