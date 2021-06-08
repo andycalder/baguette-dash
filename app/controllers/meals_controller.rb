dcd30d1cc4d5bc084b1bfb620d1e9936bf7f45d8
Julia Noiret <julia.noiret@gmail.com>
2021-06-08 14:19:28 +1000

    changed

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,8 +1,10 @@
 class MealsController < ApplicationController
   def index
+    @meals = Meal.all
   end
 
   def show
+    @meal = Meal.find(params[:id])
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Creating seeds"
+
+meals = Meal.create(name: 'Frog Sandwich', description: 'Delicious frogs in a french baguette', price: '14,9')
+meals = Meal.create(name: 'Snail Platter', description: 'Delicious varieties of snails in a french baguette', price: '40')
+meals = Meal.create(name: 'Croque-Monsieur', description: 'Delicious toasted bread with ham and cheese gruyere', price: '15')
+meals = Meal.create(name: 'Croque-Madame', description: 'Delicious toasted bread with ham, cheese gruyere and egg', price: '17')
+
+  puts "finished"
+
+
