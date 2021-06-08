092fed6b82c9e282f0155b8074235f0d9db8d187
Jorge Arias <jorgeariasgil@gmail.com>
2021-06-08 14:23:41 +1000

    Update the migration file

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,16 @@
 class OrdersController < ApplicationController
   def new
+    @meal = Meal.find(params[:meal_id])
+    @order = Order.new
+  end
+
+  def create
+    @meal = Meal.find(params[:meal_id])
+    @order = Order.new(price: @meal.price)
+    @order.meal = @meal
+    @order.user = current_user
+    @order.save
+
+    redirect_to dashboard_path(@order)
   end
 end
